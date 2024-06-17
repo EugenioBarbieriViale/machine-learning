a9b7a60e3886b3ab563d6e865b8619ca609b29af
EugenioBarbieriViale <eugenio.barbieri.viale@gmail.com>
2024-06-17 20:13:07 +0200

    new

--- and_gate.c
+++ and_gate.c
@@ -16,12 +16,12 @@ float rand_float(void) {
 	return (float) rand() / (float) RAND_MAX;
 }
 
-float loss(float w1, float w2) {
+float loss(float w1, float w2, float b) {
 	float e = 0.0f;
 	for (int i=0; i < train_count; i++) {
 		float x1 = train[i][0];
 		float x2 = train[i][1];
-		float y = w1 * x1 + w2 * x2;
+		float y = w1 * x1 + w2 * x2 + b;
 
 		e += (train[i][2] - y) * (train[i][2] - y);
 	}
@@ -31,22 +31,47 @@ float loss(float w1, float w2) {
 }
 
 int main() {
-	/* srand(time(0)); */
-	srand(438);
-	float w1 = rand_float()*3;
-	float w2 = rand_float()*5;
+	srand(59);
+	float w1 = rand_float()*1.3;
+	float w2 = rand_float()*0.9;
+	float b = rand_float()*1.2;
 	float rate = 1e-3;
 	
-	float h = 1e-3;
+	float h = 1e-4;
 
 	for (int i=0; i < 30000; i++) {
-		float dw1 = (loss(w1 + h, w2) - loss(w1, w2)) / h;
-		float dw2 = (loss(w1, w2 + h) - loss(w1, w2)) / h;
+		float dw1 = (loss(w1 + h, w2, b) - loss(w1, w2, b)) / h;
+		float dw2 = (loss(w1, w2 + h, b) - loss(w1, w2, b)) / h;
+		float db = (loss(w1, w2, b + h) - loss(w1, w2, b)) / h;
 		w1 -= rate * dw1;
 		w2 -= rate * dw2;
+		b -= rate * db;
 
-		printf("loss = %f, w1 = %f, w2 = %f\n", loss(w1, w2), w1, w2);
+		printf("loss = %f, w1 = %f, w2 = %f\n", loss(w1, w2, b), w1, w2);
 	}
+	
+	float out0 = w1 * train[0][0] + w2 * train[0][1] + b;
+    float out1 = w1 * train[1][0] + w2 * train[1][1] + b;
+    float out2 = w1 * train[2][0] + w2 * train[2][1] + b;
+    float out3 = w1 * train[3][0] + w2 * train[3][1] + b;
+
+	printf("------------------------------------------------\n");
+	printf("0 and 0: exp 0 found %f\n", out0);
+	printf("1 and 0: exp 0 found %f\n", out1);
+	printf("0 and 1: exp 0 found %f\n", out2);
+	printf("1 and 1: exp 1 found %f\n", out3);
+
+	printf("------------------------------------------------\n");
+	printf("1 for true and 0 for false: ");
+	int in0;
+	int in1;
+	scanf("%d", &in0);
+	scanf("%d", &in1);
+	float out = w1 * in0 + w2 * in1 +b;
+
+	if (out >= 0.5) out = 1;
+	else out = 0;
+	printf("%d and %d = %.0f", in0, in1, out);
 
 	return 0;
 }
